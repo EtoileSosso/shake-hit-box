b3ccbe16f10994f05e882633c1c3f268f4ec649f
Debalme Mickaël <mickael.debalme@gmail.com>
2019-11-15 11:53:11 +0100

    Initial configuration with network demo

---
+++ App-Unity/Assets/Scripts/Network.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+public class Network : MonoBehaviour
+{
+    SocketIOComponent socket;
+    JSONObject message;
+
+    void Start()
+    {
+        socket = GameObject.Find("SocketIO").GetComponent<SocketIOComponent>();
+        message = new JSONObject();
+
+        // This line will set up the listener function
+        socket.On("connectionEstabilished", onConnectionEstabilished);
+    }
+
+    // This is the listener function definition
+    void onConnectionEstabilished(SocketIOEvent evt)
+    {
+        Debug.Log("Player is connected: " + evt.data.GetField("id"));
+
+        message.AddField("message", "Hello! I am " + socket.sid);
+        socket.Emit("main", message);
+        message.Clear();
+    }
+}
---
+++ App-Unity/Assets/SocketIO/Scripts/Connection.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+public class Connection : MonoBehaviour
+{
+    private SocketIOComponent socket;
+    JSONObject message;
+
+    public void Start()
+    {
+        socket = GetComponent<SocketIOComponent>();
+        message = new JSONObject();
+        socket.On("connectionEstablished", onConnectionEstabilished);
+        socket.On("doAction", onActions);
+    }
+
+    void onConnectionEstabilished(SocketIOEvent evt)
+    {
+        Debug.Log("Player is connected: " + evt.data.GetField("id"));
+    }
+
+    public void onActions(SocketIOEvent evt)
+    {
+        Debug.Log("test");
+        Debug.Log(evt.data);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            message.AddField("message", "Hello! I am " + socket.sid);
+            socket.Emit("actions", message);
+            message.Clear();
+        }
+    }
+}
