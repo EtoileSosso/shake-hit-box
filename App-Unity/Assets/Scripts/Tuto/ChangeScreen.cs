a026793e4b4acbe7f7ac4a0ce02e76c1b80c2605
EtoileSosso <sossissa@gmail.com>
2019-11-29 09:40:15 +0100

    Tutorial

--- App-Unity/Assets/Scripts/Tuto/ChangeScreen.cs
+++ App-Unity/Assets/Scripts/Tuto/ChangeScreen.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ChangeScreen : MonoBehaviour
 {
     private int slide = 0;
+    private int tuto = 0;
+    public Animator[] anims = new Animator[4];
+    public GameObject[] tutos = new GameObject[4];
     public GameObject[] slides = new GameObject[7]; 
     public GameObject logo; 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,23 +21,55 @@ public class ChangeScreen : MonoBehaviour
             s.SetActive(false);
         }
         slides[slide].SetActive(true);
+        foreach(GameObject t in tutos)
+        {
+            t.GetComponentInChildren<Text>().color = new Color(1, 1, 1, 0.5f);
+            t.GetComponentInChildren<Image>().color = new Color(1, 1, 1, 0.5f);
+        }
+        tutos[0].GetComponentInChildren<Text>().color = new Color(1, 1, 1, 1);
+        tutos[0].GetComponentInChildren<Image>().color = new Color(1, 1, 1, 1);
+        foreach(Animator a in anims)
+        {
+            a.enabled = false;
+        }
+        anims[0].enabled = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-
         if (Input.GetButtonDown("Fire1"))
         {
             if (slide < 7)
             {
-                slides[slide].SetActive(false);
-                slide += 1;
-                slides[slide].SetActive(true);
-
-                if(slide == 6)
+                if (slide == 1)
                 {
-                    logo.SetActive(false);
+                    if (tuto < 3)
+                    {
+                        tutos[tuto].GetComponentInChildren<Text>().color = new Color(1, 1, 1, 0.5f);
+                        tutos[tuto].GetComponentInChildren<Image>().color = new Color(1, 1, 1, 0.5f);
+                        anims[tuto].enabled = false;
+                        tuto += 1;
+                        tutos[tuto].GetComponentInChildren<Text>().color = new Color(1, 1, 1, 1);
+                        tutos[tuto].GetComponentInChildren<Image>().color = new Color(1, 1, 1, 1);
+                        anims[tuto].enabled = true;
+                    }
+                    else
+                    {
+                        slides[slide].SetActive(false);
+                        slide += 1;
+                        slides[slide].SetActive(true);
+                    }
+                } else
+                {
+                    slides[slide].SetActive(false);
+                    slide += 1;
+                    slides[slide].SetActive(true);
+
+                    if (slide == 6)
+                    {
+                        logo.SetActive(false);
+                    }
                 }
             }
             else
